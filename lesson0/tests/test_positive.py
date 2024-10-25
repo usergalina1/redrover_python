ca236807b3fb81fae219ab3233a285b55b24f828
usergalina1 <usergalina1@gmail.com>
2024-10-24 23:20:13 -0700

    update imports

--- lesson0/tests/test_positive.py
+++ lesson0/tests/test_positive.py
@@ -35,27 +35,30 @@ def test_single_user():
 
 
 # Для большей читаемости можем написать первые кастомные assertions.
-# Или использовать готовую библиотеку для сравнений. 
+# Или использовать готовую библиотеку для сравнений.
+
 
 # Кастомный пример
 def should_be_equal(first, second):
     assert first == second, "Значения не совпадают"
 
+
 def test_register():
     response = request(
         method="POST",
         url="https://reqres.in/api/register",
-        # Отправляем JSON так как этого требуте наше api. 
+        # Отправляем JSON так как этого требуте наше api.
         # Так же с помощью requests можем отправлять data, files и params при необходимости.
         json={"email": "eve.holt@reqres.in", "password": "pistol"},
     )
 
-    # Можем использовать HTTPStatus или просто указать статус код как число. 
+    # Можем использовать HTTPStatus или просто указать статус код как число.
     # HTTPStatus.OK или 200
-    should_be_equal(response.status_code, HTTPStatus.OK) 
+    should_be_equal(response.status_code, HTTPStatus.OK)
     should_be_equal(response.json(), {"id": 4, "token": "QpwL5tke4Pnpja7X4"})
 
-# На данном этапе уже понятно, что тестовые данные стоит хранить отдельно от тестов.  
+
+# На данном этапе уже понятно, что тестовые данные стоит хранить отдельно от тестов.
 list_of_users_response = {
     "page": 2,
     "per_page": 6,
@@ -67,48 +70,48 @@ list_of_users_response = {
             "email": "michael.lawson@reqres.in",
             "first_name": "Michael",
             "last_name": "Lawson",
-            "avatar": "https://reqres.in/img/faces/7-image.jpg"
+            "avatar": "https://reqres.in/img/faces/7-image.jpg",
         },
         {
             "id": 8,
             "email": "lindsay.ferguson@reqres.in",
             "first_name": "Lindsay",
             "last_name": "Ferguson",
-            "avatar": "https://reqres.in/img/faces/8-image.jpg"
+            "avatar": "https://reqres.in/img/faces/8-image.jpg",
         },
         {
             "id": 9,
             "email": "tobias.funke@reqres.in",
             "first_name": "Tobias",
             "last_name": "Funke",
-            "avatar": "https://reqres.in/img/faces/9-image.jpg"
+            "avatar": "https://reqres.in/img/faces/9-image.jpg",
         },
         {
             "id": 10,
             "email": "byron.fields@reqres.in",
             "first_name": "Byron",
             "last_name": "Fields",
-            "avatar": "https://reqres.in/img/faces/10-image.jpg"
+            "avatar": "https://reqres.in/img/faces/10-image.jpg",
         },
         {
             "id": 11,
             "email": "george.edwards@reqres.in",
             "first_name": "George",
             "last_name": "Edwards",
-            "avatar": "https://reqres.in/img/faces/11-image.jpg"
+            "avatar": "https://reqres.in/img/faces/11-image.jpg",
         },
         {
             "id": 12,
             "email": "rachel.howell@reqres.in",
             "first_name": "Rachel",
             "last_name": "Howell",
-            "avatar": "https://reqres.in/img/faces/12-image.jpg"
-        }
+            "avatar": "https://reqres.in/img/faces/12-image.jpg",
+        },
     ],
     "support": {
         "url": "https://reqres.in/#support-heading",
-        "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
-    }
+        "text": "To keep ReqRes free, contributions towards server costs are appreciated!",
+    },
 }
 
 
@@ -117,12 +120,11 @@ def test_list_of_users():
         method="GET",
         # Передаем query string или строку запроса ?page=2
         url="https://reqres.in/api/users?page=2",
-        # Так же можно передать таким образом. 
+        # Так же можно передать таким образом.
         # params={"page": 2},
-        # Пагинация: Обычно используется для указания на то, какую страницу данных хотят получить. 
+        # Пагинация: Обычно используется для указания на то, какую страницу данных хотят получить.
         # Например, при разбиении списка результатов на страницы.
         # page=2: параметр для второй страницы.
-
     )
 
     should_be_equal(response.status_code, HTTPStatus.OK)
